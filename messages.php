c7eeb33cfe84c09ff4e2053e5a4e0ebc954e5212
MyPatoche <remi.de-buyser.auditeur@lecnam.net>
2023-06-21 11:01:08 +0200

    feat: get all messages on all page

--- messages.php
+++ messages.php
@@ -12,6 +12,7 @@ preg_match("/data-pagination\-button\-last\-page=\"(.*)\" data\-pagination/", $p
 
 $nbPages = intval($matches[1]);
 $nbforum = 0;
+$nbMessages = 0;
 
 $nbPages = 1; // pour tester
 
@@ -23,37 +24,40 @@ for ($i = 1; $i <= $nbPages; $i++) {
     for($j = 0; $j < count($matches[1]); $j++) {
         $forumURL = "https://www.boursorama.com".$matches[1][$j];
         //print($forumURL . "\n");
+
+        $pageForum = file_get_contents($forumURL);
+        preg_match_all("/pagination__content.*>(\d*)<\/span>/", $pageForum, $matchesNbPages);
+
+        if(sizeof($matchesNbPages[1]) == 0) {
+            $nbPagesForum = 1;
+        } else {
+            $nbPagesForum = intval($matchesNbPages[1]);
+        }
+
+        for ($k = 1; $k <= $nbPagesForum; $k++) {
+            $pageForum = file_get_contents($forumURL."page-".$k);
+
+            // retire les saut de ligne
+            $pageForum = str_replace("\n", "", $pageForum);
+            // retire les </br>
+            $pageForum = str_replace("<br />", "", $pageForum);
+            $pageForum = preg_replace("# {2,}#"," ",preg_replace("#(\r\n|\n\r|\n|\r)#"," ",$pageForum));
+            // recupere les nom des utilisateurs, les dates et les messages
+            preg_match_all("/<button.*?data\-popover\-url=\"\/espace\-membres\/profil\/(.*?)\/.*?\/button>.*?c-source__time\">(.*?)<\/span>.*?message__text.*?>(.*?)<\/p>/", $pageForum, $matches3);
+
+            // affiche les resultats
+            for($l = 0; $l < count($matches3[1]); $l++) {/*
+                print("nom : ".$matches3[1][$l]."\n");
+                print("date : ".$matches3[2][$l]."\n");
+                print("message : ".$matches3[3][$l]."\n");
+                print("\n");*/
+                $nbMessages++;
+            }
+        }
         $nbforum++;
     }
 }
 
 print("nb forum : ".$nbforum."\n");
 
-print($forumURL."\n");
-// https://www.boursorama.com/bourse/forum/AAPL/detail/432051512/
-$pageForum = file_get_contents($forumURL);
-preg_match_all("/pagination__content.*>(\d*)<\/span>/", $pageForum, $matchesNbPages);
-
-var_dump($matchesNbPages[1]);
-if(sizeof($matchesNbPages[1]) == 0) {
-    $nbPagesForum = 1;
-} else {
-    $nbPagesForum = intval($matchesNbPages[1]);
-}
-
-// retire les saut de ligne
-$pageForum = str_replace("\n", "", $pageForum);
-// retire les </br>
-$pageForum = str_replace("<br />", "", $pageForum);
-$pageForum = preg_replace("# {2,}#"," ",preg_replace("#(\r\n|\n\r|\n|\r)#"," ",$pageForum));
-// recupere les nom des utilisateurs, les dates et les messages
-preg_match_all("/<button.*?data\-popover\-url=\"\/espace\-membres\/profil\/(.*?)\/.*?\/button>.*?c-source__time\">(.*?)<\/span>.*?message__text.*?>(.*?)<\/p>/", $pageForum, $matches3);
-
-// affiche les resultats
-/*
-for($i = 0; $i < count($matches3[1]); $i++) {
-    print("nom : ".$matches3[1][$i]."\n");
-    print("date : ".$matches3[2][$i]."\n");
-    print("message : ".$matches3[3][$i]."\n");
-    print("\n");
-}*/
+print("nb messages : ".$nbMessages."\n");
